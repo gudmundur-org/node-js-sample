624c4bdd2cca36b5193e0d25391b0d052c66fc86
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2015-11-19 13:09:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello forked status morning!')
+  response.send('Hello forked status push afternoon!')
 })
 
 app.listen(app.get('port'), function() {
