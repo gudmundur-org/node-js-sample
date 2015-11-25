19de9dd4b9abaa1a76ee4361724979166ed736fd
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2015-11-25 11:25:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello forked status auto deploy at home before noon!')
+  response.send('Hello forked status auto deploy at home before noon!1')
 })
 
 app.listen(app.get('port'), function() {
